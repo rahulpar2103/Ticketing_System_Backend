4270c31645492cf6ed8153c6f2b7ce07b1e6261e
Anonymous21-03 <160504530+Anonymous21-03@users.noreply.github.com>
2026-05-23 15:19:58 +0530

    Configure production environment

--- app/db/database.py
+++ app/db/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import NullPool
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from app.core.config import settings
@@ -5,12 +6,11 @@ from sqlalchemy.orm import declarative_base
 
 Base=declarative_base()
 
-engine=create_engine(
-    settings.DATABASE_URL, 
-    pool_size=settings.DB_POOL_SIZE, 
-    max_overflow=settings.DB_MAX_OVERFLOW,
+engine = create_engine(
+    settings.DATABASE_URL,
+    poolclass=NullPool,
     echo=settings.DEBUG
-    )
+)
 session_local=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 
